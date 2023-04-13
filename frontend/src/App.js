cef70483249809ac039068654fbb22b6989daf79
Vikram <velango@ucsb.edu>
2023-04-13 15:13:08 -0700

    ve- added route in App.js for Tampa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import HomePage from "main/pages/HomePage";
 import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
-
+import TampaPage from "main/pages/TampaPage";
 import "bootstrap/dist/css/bootstrap.css";
+import SantaCruzPage from "main/pages/SantaCruz";
 
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
-        <Route exact path="/towns/SantaCruz" element={<SantaCruz />} />
+        <Route exact path="/towns/SantaCruz" element={<SantaCruzPage />} />
+        <Route exact path="/towns/Tampa" element={<TampaPage />} />
+
       </Routes>
     </BrowserRouter>
   );
