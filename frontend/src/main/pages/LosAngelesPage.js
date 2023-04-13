f712c9316090b65b1fb186de5965f48317fbe740
Elijah <ElijahFrankle626@gmail.com>
2023-04-13 14:00:17 -0700

    ef - added menu option for Los Angeles

--- frontend/src/main/pages/LosAngelesPage.js
+++ frontend/src/main/pages/LosAngelesPage.js
@@ -6,7 +6,7 @@ export default function LosAngelesPage() {
       <div className="pt-2">
         <h1>LosAngeles, CA</h1>
         <p>
-         <b>Los Angeles, California</b> is the second largest city in the United States with 3.8 million people. It is a hub for the film and entertainment industry.
+         <b>Los Angeles, California</b> is the second largest city in the United States with a population of 3.85 million people. It is a hub for the film and entertainment industry.
         </p>
         <p>Added by Elijah Frankle</p>
       </div>
